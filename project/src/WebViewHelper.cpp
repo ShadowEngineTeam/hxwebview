b60eac75dc99b4062beb017eec38081cc1434f06
Karim Akra <144803230+KarimAkra@users.noreply.github.com>
2026-03-31 10:54:53 +0300

    Make dispatch and bind GC & thread safe

--- project/src/WebViewHelper.cpp
+++ project/src/WebViewHelper.cpp
@@ -18,20 +18,20 @@ Dynamic hx_webview_version()
 
     hx::Anon sem = hx::Anon_obj::Create();
 
-        sem->Add(HX_CSTRING("major"), (int)ver->version.major);
-        sem->Add(HX_CSTRING("minor"), (int)ver->version.minor);
-        sem->Add(HX_CSTRING("patch"), (int)ver->version.patch);
+    sem->Add(HX_CSTRING("major"), (int)ver->version.major);
+    sem->Add(HX_CSTRING("minor"), (int)ver->version.minor);
+    sem->Add(HX_CSTRING("patch"), (int)ver->version.patch);
 
     hx::Anon out = hx::Anon_obj::Create();
 
-        const std::string vNum = ver->version_number;
-        const std::string prRel = ver->pre_release;
-        const std::string bMeta = ver->build_metadata;
+    const std::string vNum = ver->version_number;
+    const std::string prRel = ver->pre_release;
+    const std::string bMeta = ver->build_metadata;
 
-        out->Add(HX_CSTRING("version"), sem);
-        out->Add(HX_CSTRING("version_number"), String::create(vNum.c_str()));
-        out->Add(HX_CSTRING("pre_release"), String::create(prRel.c_str()));
-        out->Add(HX_CSTRING("build_metadata"), String::create(bMeta.c_str()));
+    out->Add(HX_CSTRING("version"), sem);
+    out->Add(HX_CSTRING("version_number"), String::create(vNum.c_str()));
+    out->Add(HX_CSTRING("pre_release"), String::create(prRel.c_str()));
+    out->Add(HX_CSTRING("build_metadata"), String::create(bMeta.c_str()));
 
     return out;
 }
@@ -42,32 +42,59 @@ void *hx_get_native_handle(webview_t w, int kind)
 {
     switch (kind)
     {
-        case 0: return webview_get_native_handle(w, WEBVIEW_NATIVE_HANDLE_KIND_UI_WINDOW);
-        case 1: return webview_get_native_handle(w, WEBVIEW_NATIVE_HANDLE_KIND_UI_WIDGET);
-        case 2: return webview_get_native_handle(w, WEBVIEW_NATIVE_HANDLE_KIND_BROWSER_CONTROLLER);
-        default: return nullptr;
+    case 0:
+        return webview_get_native_handle(w, WEBVIEW_NATIVE_HANDLE_KIND_UI_WINDOW);
+    case 1:
+        return webview_get_native_handle(w, WEBVIEW_NATIVE_HANDLE_KIND_UI_WIDGET);
+    case 2:
+        return webview_get_native_handle(w, WEBVIEW_NATIVE_HANDLE_KIND_BROWSER_CONTROLLER);
+    default:
+        return nullptr;
     }
     return nullptr;
 }
 
-// Wrapper for webview_dispatch
-using hxDispatchFunc = std::function<void(webview_t, Dynamic)>;
-
-void hx_webview_dispatch(webview_t w, hxDispatchFunc fn, Dynamic arg)
+void hx_webview_bind(webview_t w, const char *name, Dynamic fn, Dynamic farg)
 {
-    static_cast<webview::webview *>(w)->dispatch([=]() { fn(w, arg); });
-}
+    Dynamic *fnHeap   = new Dynamic(fn);
+    Dynamic *fargHeap = new Dynamic(farg);
 
-// Wrapper for webview_bind
-using hxBindFunc = std::function<void(String, String, Dynamic)>;
+    hx::GCAddRoot(&fnHeap->mPtr);
+    hx::GCAddRoot(&fargHeap->mPtr);
 
-void hx_webview_bind(webview_t w, const char *name, hxBindFunc fn, Dynamic farg)
-{
+    
     static_cast<webview::webview *>(w)->bind(
-        name, 
+        name,
         [=](const std::string &seq, const std::string &req, void *arg)
         {
-            fn(String::create(seq.c_str()), String::create(req.c_str()), farg);
-        }, 
+            hx::SetTopOfStack((int *)99, true);
+            (*fnHeap)(
+                String::create(seq.c_str()),
+                String::create(req.c_str()),
+                *fargHeap
+            );
+            hx::SetTopOfStack((int *)0, true);
+        },
         nullptr);
+}
+
+void hx_webview_dispatch(webview_t w, Dynamic fn, Dynamic arg)
+{
+    Dynamic *fnHeap  = new Dynamic(fn);
+    Dynamic *argHeap = new Dynamic(arg);
+
+    hx::GCAddRoot(&fnHeap->mPtr);
+    hx::GCAddRoot(&argHeap->mPtr);
+
+    static_cast<webview::webview *>(w)->dispatch([=]()
+    {
+        hx::SetTopOfStack((int *)99, true);
+        (*fnHeap)(w, *argHeap);
+        hx::SetTopOfStack((int *)0, true);
+
+        hx::GCRemoveRoot(&fnHeap->mPtr);
+        hx::GCRemoveRoot(&argHeap->mPtr);
+        delete fnHeap;
+        delete argHeap;
+    });
 }
